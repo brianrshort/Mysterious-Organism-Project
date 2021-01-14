14bb7c00ddde3dff8ecb68bc7e170fca8754e372
Brian Short <brian.ralph.short@gmail.com>
2021-01-14 09:18:18 -0500

    Add mysterious organism factory function to main.js

--- main.js
+++ main.js
@@ -13,3 +13,38 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// A factory function that returns a new pAequor object
+const pAequorFactory = (num, arr) => {
+  return{
+    specimenNum: num,
+    dna: arr,
+    mutate() {
+      let newDNA = returnRandBase();
+      const newIndex = Math.floor(Math.random() * 15);
+      const DNAMut = this.dna[newIndex];
+      if (DNAMut === newDNA) {
+        newDNA = returnRandBase();
+        return newDNA;
+      }
+      this.dna[newIndex] = newDNA;
+      return this.dna;
+    },
+    willLikelySurvive() {
+      let theCs = this.dna.filter(function(element) {
+        return element === "C";
+      }) 
+      //console.log(theCs);
+      let theGs = this.dna.filter(function(element) {
+        return element === "G";
+      });
+      //console.log(theGs);
+      let total = theCs.length + theGs.length;
+      //console.log(total);
+      if (total / this.dna.length >= .6) {
+        return true;
+      } else {
+        return false;
+      }
+    }  
+  }
+}
